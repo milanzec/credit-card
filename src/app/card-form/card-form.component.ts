af557c7a757ba3b32a2a5eba394a3215ee75b8d3
unknown <milanzec53@gmail.com>
2023-08-31 17:18:51 +0200

    form instance

---
+++ src/app/card-form/card-form.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms'
+
+@Component({
+  selector: 'app-card-form',
+  templateUrl: './card-form.component.html',
+  styleUrls: ['./card-form.component.css']
+})
+export class CardFormComponent {
+
+  cardForm = new FormGroup({
+    name: new FormControl('Marko Markovic')
+  })
+
+}
